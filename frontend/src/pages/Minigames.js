e80e504e0ceeb119799b5f4c7965159c9ae89884
osid54 <osid.potato@gmail.com>
2026-09-11 23:26:38 -0400

    Added bottom padding for mobile

--- frontend/src/pages/Minigames.js
+++ frontend/src/pages/Minigames.js
@@ -47,9 +47,7 @@ export default function Minigames() {
         }}
       />
 
-      <div
-        className={`absolute z-10 w-full h-full flex justify-center items-center ${isMobilePortrait ? "top-2" : "-top-4"}`}
-      >
+      <div className={`absolute z-10 w-full h-full flex justify-center items-center pb-10`}>
         <div
           className="flex flex-col items-center justify-center"
           style={{
